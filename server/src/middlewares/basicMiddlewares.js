c346e4841d98944df3b0ec678eaa80a237a2a2a7
Roman Ratushnyi <remenok9@gmail.com>
2023-11-01 20:23:34 +0200

    Fix (after review)

--- server/src/middlewares/basicMiddlewares.js
+++ server/src/middlewares/basicMiddlewares.js
@@ -6,13 +6,13 @@ const CONSTANTS = require('../constants');
 
 module.exports.parseBody = (req, res, next) => {
   req.body.contests = JSON.parse(req.body.contests);
-  for (let i = 0; i < req.body.contests.length; i++) {
-    if (req.body.contests[ i ].haveFile) {
-      const file = req.files.splice(0, 1);
-      req.body.contests[ i ].fileName = file[ 0 ].filename;
-      req.body.contests[ i ].originalFileName = file[ 0 ].originalname;
-    }
-  }
+  // for (let i = 0; i < req.body.contests.length; i++) {
+  //   if (req.body.contests[ i ].haveFile) {
+  //     const file = req.files.splice(0, 1);
+  //-->>     req.body.contests[ i ].fileName = file[ 0 ].filename;
+  //-->>     req.body.contests[ i ].originalFileName = file[ 0 ].originalname;
+  //   }
+  // }
   next();
 };
 
